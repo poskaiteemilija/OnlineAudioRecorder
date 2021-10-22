da3f361c490a60760b0ea13b2aa10ce81a9788d3
ep19927 <ep19927@essex.ac.uk>
2021-10-22 19:09:43 +0100

    added a react-howler package for playing back audio

--- website/fornt-end/src/components/Record.js
+++ website/fornt-end/src/components/Record.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import AudioReactRecorder, { RecordState } from 'audio-react-recorder'; //audio recording npm library
 
 //the code in record button is referenced from sample code on https://www.npmjs.com/package/audio-react-recorder    
@@ -6,9 +6,15 @@ import AudioReactRecorder, { RecordState } from 'audio-react-recorder'; //audio 
 let RecordButton = () =>{
     const [recState, setRecord] = useState("");
 
+    const onStop = useCallback((audioData) => {
+        console.log('audio', audioData);
+        const audio = new Audio(audioData.url);
+        audio.play();
+    });
+
     return(
         <div className="recordButton">
-            <AudioReactRecorder state={ recState } onStop={ (audioData) => console.log('audio', audioData) }/>
+            <AudioReactRecorder state={ recState } onStop={ onStop }/>
             <button onClick={() => setRecord(RecordState.START)}>Record</button>
             <button onClick={() => setRecord(RecordState.STOP)}>Stop</button>
         </div>
