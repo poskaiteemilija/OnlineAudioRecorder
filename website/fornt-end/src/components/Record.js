04bb2b71447256a9c2ca081e150a7631cdf996fd
ep19927 <ep19927@essex.ac.uk>
2021-10-25 20:39:32 +0100

    stored audio blob in local storage and audio state variable

--- website/fornt-end/src/components/Record.js
+++ website/fornt-end/src/components/Record.js
@@ -1,15 +1,21 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useState, useEffect } from 'react';
 import AudioReactRecorder, { RecordState } from 'audio-react-recorder'; //audio recording npm library
+import ReactHowler from 'react-howler';
 
 //the code in record button is referenced from sample code on https://www.npmjs.com/package/audio-react-recorder    
 
 let RecordButton = () =>{
     const [recState, setRecord] = useState("");
+    const [audioState, setAudio] = useState();
 
     const onStop = useCallback((audioData) => {
-        console.log('audio', audioData);
-        const audio = new Audio(audioData.url);
-        audio.play();
+        console.log('audio', audioData, audioData.url);
+        localStorage.setItem('bloblink', audioData.url);
+    });
+
+    useEffect(() => {
+        setAudio(localStorage.getItem('bloblink'));
+        console.log(audioState);
     });
 
     return(
@@ -20,5 +26,7 @@ let RecordButton = () =>{
         </div>
     );
 }
+
+
 
 export default RecordButton;
