9c933b5fb88bf8fb38853767ec31cd42fe07b144
ep19927 <ep19927@essex.ac.uk>
2021-12-10 21:53:14 +0000

    discovered bug with webm format using pydub, using ffmpeg and console commands instead

--- website/backend/back/encoder.py
+++ website/backend/back/encoder.py
@@ -1,5 +1,10 @@
 from pydub import AudioSegment
+import subprocess
 
-test_audio = AudioSegment.from_file("D:\\Bakalauras\\CE301_poskaite_emilija\\website\\backend\\untitled.mp3", format="mp3")
 
-test_audio.export("test.wav", format="wav")
+
+#test_audio = AudioSegment.from_file("D:\\Bakalauras\\CE301_poskaite_emilija\\website\\backend\\untitled.mp3", format="mp3")
+#
+#test_audio.export("test.wav", format="wav")
+
+subprocess.run(['ffmpeg', '-i', "D:\\Bakalauras\\CE301_poskaite_emilija\\website\\backend\\r_gguWa6J.webm", "D:\\Bakalauras\\CE301_poskaite_emilija\\website\\backend\\test3.mp3"])
