a0af725268d4d18541013858e6b498186a43aa4d
ep19927 <ep19927@essex.ac.uk>
2021-12-10 21:56:59 +0000

    nevermind, for whatever reason the bug does not exists after restoring the previous version of the project and pydub decided to work with webm

--- website/backend/back/encoder.py
+++ website/backend/back/encoder.py
@@ -3,8 +3,8 @@ import subprocess
 
 
 
-#test_audio = AudioSegment.from_file("D:\\Bakalauras\\CE301_poskaite_emilija\\website\\backend\\untitled.mp3", format="mp3")
-#
-#test_audio.export("test.wav", format="wav")
+test_audio = AudioSegment.from_file("D:\\Bakalauras\\CE301_poskaite_emilija\\website\\backend\\r_5IsDWVB.webm")
 
-subprocess.run(['ffmpeg', '-i', "D:\\Bakalauras\\CE301_poskaite_emilija\\website\\backend\\r_gguWa6J.webm", "D:\\Bakalauras\\CE301_poskaite_emilija\\website\\backend\\test3.mp3"])
+test_audio.export("test5.mp3", format="mp3")
+
+#subprocess.run(['ffmpeg', '-i', "D:\\Bakalauras\\CE301_poskaite_emilija\\website\\backend\\r_gguWa6J.webm", "D:\\Bakalauras\\CE301_poskaite_emilija\\website\\backend\\test3.mp3"])
