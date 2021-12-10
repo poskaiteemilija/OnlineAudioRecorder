ff2517a16acf2d2575c24774e5b97c02955e3dea
ep19927 <ep19927@essex.ac.uk>
2021-12-10 22:03:41 +0000

    moved encoding to a function

--- website/backend/back/encoder.py
+++ website/backend/back/encoder.py
@@ -3,8 +3,14 @@ import subprocess
 
 
 
-test_audio = AudioSegment.from_file("D:\\Bakalauras\\CE301_poskaite_emilija\\website\\backend\\r_5IsDWVB.webm")
+def convert_audio(source, selected_format, filename):
+    webm_audio = AudioSegment.from_file(source)
+    webm_audio.export(filename, format=selected_format)
 
-test_audio.export("test5.mp3", format="mp3")
+
+#test_audio = AudioSegment.from_file("D:\\Bakalauras\\CE301_poskaite_emilija\\website\\backend\\r_5IsDWVB.webm")
+#test_audio.export("test5.mp3", format="mp3")
 
 #subprocess.run(['ffmpeg', '-i', "D:\\Bakalauras\\CE301_poskaite_emilija\\website\\backend\\r_gguWa6J.webm", "D:\\Bakalauras\\CE301_poskaite_emilija\\website\\backend\\test3.mp3"])
+
+convert_audio("D:\\Bakalauras\\CE301_poskaite_emilija\\website\\backend\\r_5IsDWVB.webm", "mp3", "test6.mp3")
